3526085594a345640fabb2b5ec5dca596eaeccb6
Evandro Rhari <93932260+E-rhari@users.noreply.github.com>
2024-04-17 18:10:33 -0300

    Indicadores de acerto de beat
    Ao acertar ou errar um beat, será indicado através de um sprite em cima do hit.
    Os sprites foram feitos as pressas e são todos placeholders

---
+++ GamethONS_Case/Assets/Scripts/RythmScripts/HitDisplay.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDisplay : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Coroutine currentCoroutine;
+    private bool isCoroutineRunning = false;
+
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update(){
+        
+    }
+
+
+    public void SetTemporarySprite(Sprite sprite, float waitTime=1f)
+    {
+        ChangeSprite(sprite);
+        isCoroutineRunning = true;
+        currentCoroutine = StartCoroutine(FadeOut(waitTime));
+        isCoroutineRunning = false;
+
+    }
+
+    public void ChangeSprite(Sprite sprite)
+    {
+        if(isCoroutineRunning)
+        {
+            isCoroutineRunning = false;
+            StopCoroutine(currentCoroutine);
+        }
+        spriteRenderer.color = new Color32(255, 255, 255, 255);
+        spriteRenderer.sprite = sprite;
+    }
+
+
+    private IEnumerator FadeOut(float waitTime=0.5f)
+    {
+        float waitPerLoop = waitTime/255f;
+        Color32 lessAlpha = new Color32(0,0,0,1);
+        while(spriteRenderer.color != new Color32(255,255,255,0))
+        {
+            spriteRenderer.color -= lessAlpha;
+            yield return new WaitForSecondsRealtime(waitPerLoop);
+        }
+    }
+}
