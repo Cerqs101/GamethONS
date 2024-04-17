28c5d53b216633a0bbb2eadf02786cac23dd557b
Gab <gaspargabrielpv@gmail.com>
2024-04-17 10:03:58 -0300

    update barra de música

--- GamethONS_Case/Assets/Scripts/RythmScripts/musicBar.cs
+++ GamethONS_Case/Assets/Scripts/RythmScripts/musicBar.cs
@@ -5,13 +5,38 @@ using UnityEngine.UI;
 public class musicBar : MonoBehaviour
 {
     // Start is called before the first frame update
+    bool isToSetMusicBar = true;
     public Slider slider;
-    public void setMaxHeath(){
-        // LaneObject[] lanes = FindObjectsByType<LaneObject>(FindObjectsSortMode.None);
-        // foreach(LaneObject lane in lanes)
-        //     slider.maxValue = lane.instance.histPerEncounter()*2;
+    public GameObject musicBarObject;
+    public void setMaxValueMusicBar(){
+        LaneObject[] lanes = FindObjectsByType<LaneObject>(FindObjectsSortMode.None);
+        foreach(LaneObject lane in lanes)
+            slider.maxValue += lane.histPerEncounter()*2;
     }
-    public void SetMusicBar(){
-        slider.value =  LevelManager.hits - LevelManager.misses;
+    public void SetValueMusicBar(){
+        slider.value =  slider.maxValue/2 + (LevelManager.hits - LevelManager.misses);
+        Debug.Log(LevelManager.misses);
+        Debug.Log(LevelManager.hits);
+        Debug.Log(slider.maxValue/2 + (LevelManager.hits - LevelManager.misses));
+    }
+    void Start(){
+      // musicBarObject.SetActive(false);
+    }
+    void Update(){
+        if(LevelManager.isEncounterHappening){
+            musicBarObject.SetActive(true);
+            if(isToSetMusicBar){
+                setMaxValueMusicBar();
+                isToSetMusicBar = false;
+            }
+            SetValueMusicBar();
+        }
+        if(!LevelManager.isEncounterHappening){
+            isToSetMusicBar = true;
+            slider.maxValue = 0;
+            //usicBarObject.SetActive(false);
+        }
+            
+
     }
 }
